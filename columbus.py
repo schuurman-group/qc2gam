1f1d4e4c5db5bd8411f1e1e177053b7621bf26c7
Michael Schuurman <schuurm@saturn.sao.nrc.ca>
2018-11-10 22:50:24 -0500

    starting work on columbus parser

--- columbus.py
+++ columbus.py
@@ -5,27 +5,67 @@
 #
 
 import os
+import moinfo
 
+def parse(geom_file, basis_file, mo_file):
+    """Documentation to come"""
 
-def parse(mo_file, dalt_file, geom_file):
+    # read columbus geometry file
+    gam_geom  = read_geom(geom_file)
+    
+    # parse daltaoin file to get basis information
+    gam_basis = read_basis(basis_file, gam_geom)
+
+    # parse mocoef file to pull out orbitals
+    gam_mos   = read_mos(mo_file)
+
+    return [gam_basis, gam_mos]
+
+#
+def read_geom(geom_file):
+    """reads a columbus geom and loads results into moinfo.geom format"""
+
+    # create geometry object to hold the data
+    geom = moinfo.geom()
+
+    # slurp up geometry file
+    with open(geom_file) as col_geom: 
+        gfile = col_geom.readlines()
+   
+    atoms = []
+    for i in range(len(gfile)):
+        gstr   = gfile[i].split() # format = asym, anum, coords
+        asym   = gstr[0].upper().rjust(2)
+        coords = [float(gstr[i]) for i in range(2,5)]
+        atom_i = moinfo.atom(asym,coords)
+        geom.add_atom(atom_i) 
+
+    return geom
+
+def read_basis(basis_file, geom):
+    """Documentation to come"""
+
+    # create basis set object
+    basis = moinfo.basis_set('unknown',geom)
+
+    # slurp up daltaoin file
+    with open(basis_file) as daltaoin:
+        bfile = daltaoin.readline()
+   
+    # step through daltaoin file and read functions
+    # also take note if calculation uses cartesian functions
+    for i in range(len(bfile)):
+        bline = bfile[i].split()
+        
+
 
     return
-
 
 def read_mos(mocoef_file):
+    """Documentation to come"""
+
 
     return
-
-
-def read_basis(basis_file):
-
-    return
-
-
-def read_geom(geom_file):
-
-    return
-
 
 
 
