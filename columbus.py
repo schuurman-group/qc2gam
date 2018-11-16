6442a8c6b0dd8786d0c7a86ab27a35a65c24fb16
Michael Schuurman <schuurm@saturn.sao.nrc.ca>
2018-11-16 00:12:44 -0500

    now printing GAMESS output. Still need to check the mappings/numbers a bit closer

--- columbus.py
+++ columbus.py
@@ -5,8 +5,29 @@
 #
 
 import sys
+import numpy as np
 import moinfo
 
+# DALTON orbital ordering (in cartesians)
+# s px py pz dxx dxy dxz dyy dyz dzz fxxx fxxy fxxz fxyy fxyz fxzz fyyy fyyz fyzz fzzz
+# DALTON orbital ordering (in spherical harmonics)
+# s px py pz d2- d1- d0 d1+ d2+ f3- f2- f1- f0 f1+ f2+ f3+
+ao_ordr         = [['s'],
+                   ['px','py','pz'],
+                   ['dxx','dxy','dxz','dyy','dyz','dzz'],
+                   ['fxxx','fxxy','fxxz','fxyy','fxyz',
+                    'fxzz','fyyy','fyyz','fyzz','fzzz']]
+
+ao_norm         = [[1.],
+                   [1., 1., 1.],
+                   [1./np.sqrt(3.), 1./np.sqrt(3.), 1./np.sqrt(3.), 
+                    1./np.sqrt(3.), 1./np.sqrt(3.), 1./np.sqrt(3.)],
+                   [1./np.sqrt(15.),np.sqrt(5./3.),np.sqrt(5./3.),
+                    np.sqrt(5./3.),np.sqrt(15.),1/np.sqrt(5./3.),
+                    1./np.sqrt(15.),np.sqrt(5./3.),np.sqrt(5./3.),
+                    1./np.sqrt(15.)]]
+
+#
 def parse(geom_file, basis_file, mo_file):
     """Documentation to come"""
 
@@ -17,7 +38,7 @@ def parse(geom_file, basis_file, mo_file):
     [in_cart, gam_basis] = read_basis(basis_file, gam_geom)
 
     # parse mocoef file to pull out orbitals
-    gam_mos   = read_mos(in_cart, mo_file)
+    gam_mos   = read_mos(mo_file, in_cart, gam_basis)
 
     return [gam_basis, gam_mos]
 
@@ -48,29 +69,34 @@ def read_basis(basis_file, geom):
     # create basis set object
     basis = moinfo.basis_set('unknown',geom)
 
+    # initialize the ordering array
+    dalt_gam_map = [[] for i in range(geom.natoms())]
+
     # slurp up daltaoin file
     with open(basis_file) as daltaoin:
         bfile = daltaoin.readlines()
    
     # step through daltaoin file and read functions
-
     # first thing to check: cartesians/spherical and how many
     # groups of contractions
     i = 0
-    while(bfile[i].split()[0].lower() != 'c' and 
-               bfile[i].split()[0].lower() != 's'):
+    while True:
+        if len(bfile[i].split())>0:
+            if (bfile[i].split()[0].lower() == 'c' or
+               bfile[i].split()[0].lower() == 's'):
+                break   
+
         i+=1
 
-    in_cartestians = (bfile[i].split()[0].lower() == 'c'
-    n_grp          = int(bfile[i].split()[1])
+    in_cartesians = bfile[i].split()[0].lower() == 'c'
+    n_grp         = int(bfile[i].split()[1])
 
     # iterate over the number of groups
-    i+=1
     atm_cnt = 0
     for j in range(n_grp):
- 
-        [a_num, n_atm, n_shl] = bfile[i].split()[0:3]
-        n_per_shell           = bfile[i].split()[3:]
+        i += 1
+        [n_atm,n_shl]         = list(map(int,bfile[i].split()[1:3]))
+        n_per_shell           = list(map(int,bfile[i].split()[3:]))
  
         # for time being assume C1, so number of atoms in
         # daltaoin file == number of atoms in geom file
@@ -86,12 +112,13 @@ def read_basis(basis_file, geom):
             ang_mom += 1
             for l in range(n_per_shell[k]):
                 i+=1 
-                [int_prog, n_prim, n_con] = bfile[i].split()
+                [n_prim, n_con] = list(map(int,bfile[i].split()[1:]))
                 b_funcs = [moinfo.basis_function(ang_mom) for m in range(n_con)]
                 for m in range(n_prim):
                     i+=1
-                    expon = float(bfile[i].split()[0])
-                    coefs = bfile[i].split()[1:]
+                    exp_con = list(map(float,bfile[i].split()))
+                    expon = exp_con[0]
+                    coefs = exp_con[1:]
                     for n in range(n_con):
                         b_funcs[n].add_primitive(expon, coefs[n])
                 for m in range(n_atm):
@@ -103,11 +130,68 @@ def read_basis(basis_file, geom):
 
     return [in_cartesians, basis]
 
-def read_mos(mocoef_file):
+def read_mos(mocoef_file, in_cart, basis):
     """Documentation to come"""
 
+    # So: in the future, we should also allow for a mapping array that
+    # allows for taking linear combinations of elements in order to convert
+    # from a spherically adapted basis to cartesians. That will come later
+    # though...first the easy stuff.    
 
-    return
+    # slurp up the mocoef file
+    with open(mocoef_file) as mocoef:
+        mo_file = mocoef.readlines()
+
+    # move to the first line of the orbital coefficients, reading nao and nmo
+    # along the way
+    line_index = 0
+    while(mo_file[line_index].split()[0][0] != '('):
+        line_index += 1
+        # figure out nao, nmo
+        if mo_file[line_index].split()[0] == "A":
+            [nao, nmo] = list(map(int,mo_file[line_index-1].split()))
+
+    # create an orbital object to hold orbitals
+    gam_orb = moinfo.orbitals(nao, nmo)
+   
+    for i in range(nmo):
+        mo_vec   = np.zeros(nao, dtype=float)
+        n_remain = nao
+        row      = 0
+        while(n_remain > 0):
+            line_index += 1
+            line_arr    = mo_file[line_index].split()
+            mo_vec[3*row:3*row+min(n_remain,3)] = np.array(
+               [line_arr[k].replace("D","e") for k in range(len(line_arr))],dtype=float)             
+            n_remain -= min(n_remain,3)
+            row += 1
+        gam_orb.add(mo_vec)
+
+    # make the map array
+    nf_cnt = 0
+    dalt_gam_map = []
+    scale_col     = []
+    scale_gam     = []
+    for i in range(basis.geom.natoms()):
+        for j in range(len(basis.basis_funcs[i])):
+            ang_mom = basis.basis_funcs[i][j].ang_mom
+            nfunc   = moinfo.nfunc_per_shell[ang_mom]
+            map_arr = [moinfo.ao_ordr[ang_mom].index(ao_ordr[ang_mom][k]) for k in range(nfunc)]
+            dalt_gam_map.extend([nf_cnt + map_arr[k] for k in range(len(map_arr))])
+            scale_col.extend([1./ao_norm[ang_mom][k] for k in range(nfunc)])
+            scale_gam.extend([moinfo.ao_norm[ang_mom][k] for k in range(nfunc)])
+            nf_cnt += nfunc
+
+    # remove the dalton normalization factors
+    gam_orb.scale(scale_col)
+  
+    # re-sort orbitals to GAMESS ordering
+    gam_orb.sort(dalt_gam_map)
+
+    # apply the GAMESS normalization factors
+    gam_orb.scale(scale_gam)
+
+    return gam_orb
 
 
 
--- moinfo.py
+++ moinfo.py
@@ -8,8 +8,8 @@ import math
 import numpy as np
 
 a_symbols       = [' H','HE',
-                   'LI','BE','B ','C ','N ','O ','F ','NE',
-                   'NA','MG','AL','SI','P ','S ','CL','AR']
+                   'LI','BE',' B',' C',' N',' O',' F','NE',
+                   'NA','MG','AL','SI',' P',' S','CL','AR']
 
 a_numbers       = ['1.0','2.0',
                    '3.0','4.0','5.0','6.0','7.0','8.0','9.0','10.0',
@@ -19,10 +19,24 @@ ang_mom_sym     = ['S','P','D','F','G','H','I']
 
 nfunc_per_shell = [1, 3, 6, 10, 15, 21, 28] 
 
+ao_ordr         = [['s'],
+                   ['px','py','pz'],
+                   ['dxx','dyy','dzz','dxy','dxz','dyz'],
+                   ['fxxx','fyyy','fzzz','fxxy','fxxz',
+                    'fxyy','fyyz','fxzz','fyzz','fxyz']]
+
+ao_norm         = [[1.],
+                   [1.,1.,1.],
+                   [1.,1.,1.,1./np.sqrt(3.),1./np.sqrt(3.),1./np.sqrt(3.)],
+                   [1.,1.,1.,1./np.sqrt(5.),1./np.sqrt(5.),1./np.sqrt(5.),
+                             1./np.sqrt(5.),1./np.sqrt(5.),1./np.sqrt(5.),1./np.sqrt(15.)]]
+  
 # wrapper for all requisite data about an atom
 class atom:
+    """Documentation to come"""
 
     def __init__(self, label, coords=None):
+        """Documentation to come"""
         # atomic symbol
         self.symbol    = label
 
@@ -30,8 +44,8 @@ class atom:
         try:
             self.number = a_numbers[a_symbols.index(self.symbol)]
         except:
-            print('Atom: '+str(label)+' not found, 
-                   setting atomic number to \'0\'\n')
+            print('Atom: '+str(label)+' not found,'+ 
+                  'setting atomic number to \'0\'\n')
             self.number = '0.0'
 
         # coordinates
@@ -42,22 +56,24 @@ class atom:
 
     # define the coordinates of the atom
     def set_coords(self, coords):
+        """Documentation to come"""
         self.coords = coords
 
     # print the atom in GAMESS file format
     def print_atom(self, file_handle):
-   
+        """Documentation to come"""
         ofmt   = ('{:2s}'+'{:>13s}'+
-                  ''.join('{:18.10f}' for i in range(3))
-        w_data = [self.symbol,self.number].extend(self.coords)
-
+                  ''.join('{:18.10f}' for i in range(3))+'\n')
+        w_data = [self.symbol,self.number] + self.coords
         file_handle.write(ofmt.format(*w_data))
             
 
 # class to hold geometry information
 class geom:
+    """Documentation to come"""
 
     def __init__(self, atoms=None):
+        """Documentation to come"""
         # if atoms is None, initialize empty atom list
         if atoms is None:
             self.atoms = []
@@ -65,16 +81,25 @@ class geom:
             self.atoms = atoms
 
     def add_atom(self, atom):
-        self.atoms.extend(atom)
+        """Documentation to come"""
+        self.atoms.append(atom)
         return
 
     def natoms(self):
+        """Documentation to come"""
         return len(self.atoms)   
 
+    def print_geom(self, file_handle):
+        """Prints geometry in GAMESS format"""
+        for i in range(self.natoms()):
+            self.atoms[i].print_atom(file_handle)
+  
 # wrapper to hold information about the orbitals
 class orbitals:
+    """Documentation to come"""
 
     def __init__(self, n_aos, n_mos):
+        """Documentation to come"""
         # number of AOs
         self.naos       = n_aos
         # number of MOs
@@ -84,6 +109,8 @@ class orbitals:
 
     # swap two orbitals
     def swap(self, mo_i, mo_j):
+        """Documentation to come"""
+
         if max(mo_i,mo_j) <= self.mo_vectors.shape[1]:
             mo_tmp = self.mo_vectors[:,mo_i]
             self.mo_vectors[:,mo_i] = self.mo_vectors[:,mo_j]
@@ -92,42 +119,60 @@ class orbitals:
 
     # add an orbital to the end of the list (i.e. at first column with norm==0
     def add(self, mo_vec):
+        """Documentation to come"""
 
         # only add orbital if the number of aos is correct
-        if len(mo_vec) != self.n_aos:
+        if len(mo_vec) != self.naos:
             print("Cannot add orbital, wrong number of naos: "+
                    str(len(mo_vec))+"!="+str(self.naos))
             return
 
         i = 0
-        while(np.linalg.norm(self.mo_vectors(:,i)) > fpzero):
+        while(np.linalg.norm(self.mo_vectors[:,i]) > 1.e-10):
             i += 1
-        self.mo_vectors(:,i) = mo_vec
+        self.mo_vectors[:,i] = mo_vec
         return
 
     # insert an orbital
-    def insert(self, mo_vec, mo_i)):
+    def insert(self, mo_vec, mo_i):
+        """Documentation to come"""
         self.mo_vectors = np.insert(self.mo_vectors,mo_i,mo_vec,axis=1)
         return
 
     # delete an orbital
     def delete(self, mo_i):
+        """Documentation to come"""
         self.mo_vectors = np.delete(self.mo_vectors,mo_i,axis=1)
         return
 
     # scale entire MO by a scalar
-    def scale(self, mo_i, fac):
-        self.mo_vectors(:,mo_i) *= fac
+    def scale_mo(self, mo_i, fac):
+        """Documentation to come"""
+        self.mo_vectors[:,mo_i] *= fac
         return
 
     # scale each element of MO vector by a specific value
-    def scale_vec(self, mo_i, fac_vec):
-        self.mo_vectors(:,mo_i) *= fac_vec
+    def scale_mo_vec(self, mo_i, fac_vec):
+        """Documentation to come"""
+        self.mo_vectors[:,mo_i] *= fac_vec
+        return
+   
+    # scale each MO by the vector fac_vec
+    def scale(self, fac_vec):
+        """scale each MO by the vector fac_vec"""
+        self.mo_vectors = (self.mo_vectors.T * np.array(fac_vec)).T
+
+    # re-sort mos according to a map array
+    def sort(self, map_lst):
+        """re-sort the MOs, ordering the AO indices via map_lst"""
+        for i in range(self.nmos):
+            self.mo_vectors[:,i] = self.mo_vectors[map_lst,i]
         return
 
     # take the norm of an orbital
     def norm(self, mo_i):
-        np.linalg.norm(self.mo_vectors(:,mo_i))
+        """Documentation to come"""
+        np.linalg.norm(self.mo_vectors[:,mo_i])
         return
 
     # urconvert mos from spherical basis to cartesian basis
@@ -137,10 +182,10 @@ class orbitals:
 
     # print orbitals in gamess style VEC format
     def print_orbitals(self, file_name, n_orb=None):
-
+        """Documentation to come"""
         # default is to print all the orbitals
         if n_orb is None:
-            n_orb = self.nmo
+            n_orb = self.nmos
 
         # open file_name, append if file already exists
         with open(file_name, 'a') as mo_file:
@@ -153,50 +198,59 @@ class orbitals:
 
     # print an orbital vector
     def print_movec(self, file_handle, mo_i):
+        """Documentation to come"""
         n_col = 5 # this is set by GAMESS format
         n_row = int(math.ceil(self.naos/n_col))
 
-        mo_row = ('{:>2s}'+' '+'{:>2s}'+''.join('{:15.8e}' for i in range(n_col))+'\n')
+        mo_row = ('{:>2d}'+' '+'{:>2d}'+''.join('{:15.8e}' for i in range(n_col))+'\n')
         mo_lab = mo_i % 100
 
         for i in range(n_row):
-            r_data = [mo_lab,i].append(self.mo_vectors[5*i:5*(i+1),mo_i])
-            file_handle.write(mo_row.format(*r_data)
+            r_data = [mo_lab, i]
+            r_data.extend(self.mo_vectors[5*i:5*(i+1),mo_i].tolist())
+            file_handle.write(mo_row.format(*r_data))
 
         return
 
 # an object to hold information about a single basis function
 # including angular momentum, exponents and contraction coefficients
 class basis_function:
+    """Documentation to come"""
 
-    def __init_(self, ang_mom):
+    def __init__(self, ang_mom):
+        # angular momentum of basis function
         self.ang_mom = ang_mom 
+        # text symbol of the angular momentum shell
         self.ang_sym = ang_mom_sym[self.ang_mom]
+        # number of primitives in basis
         self.n_prim  = 0
+        # list of exponents
         self.exps    = []
+        # list of coefficients
         self.coefs   = []
 
     def add_primitive(self, expo, coef):
-        self.exps.extend(expo)
-        self.coefs.extend(coef)
+        """Documentation to come"""
+        self.exps.extend([expo])
+        self.coefs.extend([coef])
         self.n_prim += 1
         return
 
     def print_basis_function(self, file_handle):
-       
-        ofmt1 = ('{:1s}'+'{:>6i}'+'\n')
-        ofmt2 = ('{:>3i}'+'{:>15.7f}'+'{:>23.7f}'+'\n')
+        """Documentation to come"""
+        ofmt1 = ('{:1s}'+'{:>6d}'+'\n')
+        ofmt2 = ('{:>3d}'+'{:>15.7f}'+'{:>23.7f}'+'\n')
 
         w_data = [self.ang_sym, self.n_prim]
         file_handle.write(ofmt1.format(*w_data))
         for i in range(self.n_prim):
-            w_data = [i, self.exps[i],self.coefs[i]]
-            file_handle.write(ofmt1.format(*w_data))
+            w_data = [i+1, self.exps[i],self.coefs[i]]
+            file_handle.write(ofmt2.format(*w_data))
         return
 
 # contains the basis set information for a single atom
 class basis_set:
-
+    """Documentation to come"""
     def __init__(self, label, geom):
         # string label of basis set name
         self.label       = label
@@ -211,9 +265,9 @@ class basis_set:
 
     # add a basis function to the basis_set
     def add_function(self, atom_i, bf):
-        self.basis_funcs[atom_i].extend(bf)
-        self.n_cont[atom_i]      += 1
-        self.basis_funcs[atom_i] += n_per_shell[bf.ang_mom]
+        self.basis_funcs[atom_i].extend([bf])
+        self.n_cont[atom_i] += 1
+        self.n_func         += nfunc_per_shell[bf.ang_mom]
         return
 
     # print  the data section of a GAMESS style input file
@@ -224,14 +278,14 @@ class basis_set:
             dat_file.write('$DATA\n'+
                            'Comment line | basis='+str(self.label)+'\n')
 
-            for i in range(geom.natoms):
-                geom.atoms[i].print_atom(dat_file)
-                for j in range(self.n_cont[i])
+            for i in range(self.geom.natoms()):
+                self.geom.atoms[i].print_atom(dat_file)
+                for j in range(self.n_cont[i]):
                     self.basis_funcs[i][j].print_basis_function(dat_file)       
                 # each atomic record ends with an empty line
                 dat_file.write('\n')
 
-            mo_file.write('$END\n')
+            dat_file.write('$END\n')
 
         return
 
